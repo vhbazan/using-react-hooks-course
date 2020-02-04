cfe19edfa8ad87baa2bffb5fbaec590b16eaa2ab
vbazanm <victor_bazan_milla@optum.com>
2020-02-04 09:43:55 +0000

    setState hook: first page using an input example

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+
+const InputElement = () => {
+
+const [inputText, setInputText] = useState("");
+const [historyList, setHistoryList] = useState([]);
+
+
+    return (<div>
+            <input
+            onChange={ (e) => { 
+                setInputText(e.target.value);
+                setHistoryList([...historyList, e.target.value]) 
+            }}
+            placeholder="Enter some text" />
+            <br/>
+            <label>{inputText}</label>
+            {historyList.map(item => {
+                return <div>{item}</div>
+            })}
+            </div>);
+}
+
+export default InputElement;
