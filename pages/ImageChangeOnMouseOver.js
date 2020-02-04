4baf788d3c049f11b95b16adb09111969d7134c2
vbazanm <victor_bazan_milla@optum.com>
2020-02-04 14:23:03 +0000

    Using useRef: First b/w images basic template

---
+++ pages/ImageChangeOnMouseOver.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ImageChangeOnMouseOver = () => {
+    return (
+        <div>
+            <img src="/static/bw/Speaker-187.png" alt="speaker 187" height="300px"/>
+
+            <img src="/static/bw/Speaker-1124.png" alt="speaker 1124" height="300px"/>
+
+            <div id="footer" className="row">
+                <div className="col-12">
+                    <p className="text-muted credit">
+                    All images are from <a href="http://commons.wikimedia.org">Commons Wikimedia</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ImageChangeOnMouseOver;
