6a8ead9716d2cedada0881b2d5f590829190ad7b
vbazanm <victor_bazan_milla@optum.com>
2020-02-05 10:23:38 +0000

    useEffect: ImageChangeOnScroll feature

---
+++ pages/ImageChangeOnScroll.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ImageToggleOnScroll from '../src/ImageToggleOnScroll';
+
+
+const ImageChangeOnScroll = () => {
+    return (
+        <div>
+            {[1124, 187, 23, 56, 1124,, 187, 23, 56].map( (speakerId, index) => {
+                return (
+                    <div key={speakerId + index}>
+                        <ImageToggleOnScroll
+                            primaryImg={`/static/bw/Speaker-${speakerId}.png`}
+                            secondaryImg={`/static/Speaker-${speakerId}.png`}
+                            alt=""
+                        />
+                    </div>
+                )
+            } ) }
+            
+            <div id="footer" className="row">
+                <div className="col-12">
+                    <p className="text-muted credit">
+                    All images are from <a href="http://commons.wikimedia.org">Commons Wikimedia</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ImageChangeOnScroll;
